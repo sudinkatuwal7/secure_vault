57385d8be52fc077778afe6abb64cc5c9cb960b9
Sudin Katuwal <sudcrack2024@gmail.com>
2026-02-25 14:43:39 -0600

    Add json import for password generator

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
-
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
